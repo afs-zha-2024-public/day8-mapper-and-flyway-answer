29b9705d4f9001b14bab31c59c7f391f928b91e0
Minghao Li <minghao.li@thoughtworks.com>
2024-11-15 19:52:57 +0800

    [SpringBoot][Flyway] feat: add init flyway data migration

---
+++ src/main/resources/db/migration/V20241115931933__insert_data_into_table_employee_and_company.sql
@@ -0,0 +1,4 @@
+insert into company SET name='afs';
+
+insert into employee SET name='Jonson', age=21, gender='Male', salary=7000, company_id=1;
+insert into employee SET name='sally', age=28, gender='Female', salary=12000, company_id=1;
